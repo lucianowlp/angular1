bfdf8f1f47fc3dbc4783a894cadbc56cd771b007
Luciano Mattos <desenv.mattos@hotmail.com>
2018-01-07 13:24:34 -0200

    criando a tela de lista

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -1,21 +1,32 @@
 (function(){
     angular.module('primeiraApp').controller('billingCycleCtrl',[
         '$http',
+        'msgs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http) {
+    function BillingCycleController($http, msgs) {
         const vm = this
-        
-        vm.create = function() {
-            const url = 'http://localhost:3003/api/billingCycles'
-            $http.post(url, vm.billingCycle).then(function(response) {
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.refresh = () => {
+            $http.get(url).then(function(response) {
                 vm.billingCycle = {}
-                console.log('Sucesso!')
+                vm.billingCycles = response.data
             }, function(response) {
-                vm.billingCycle = {}
-                console.log('Falhou!')
+                msgs.addError(response.data.errors)
+            })   
+        }
+        
+        vm.create = () => {
+            $http.post(url, vm.billingCycle).then(function(response) {
+                vm.refresh()
+                msgs.addSucess('Operação realizada com sucesso!')
+            }, function(response) {
+                msgs.addError(response.data.errors)
             })
         }
+
+        vm.refresh()
     }
 })()
